b70531f4e3943f3117de1a3da0ddd81174bf3963
swhasans <SS3858@live.mdx.ac.uk>
2023-06-08 22:05:38 -0400

    Implement operate function for performing calculations with an operator and two numbers.

--- index.js
+++ index.js
@@ -2,19 +2,41 @@ const numOne = 0;
 const operator = "";
 const numTwo = 0;
 
+const operate = function (operator, numOne, numTwo) {
 
-const add = function (num1, num2) {
+    if(operator === "+")
+    {
+        return numOne + numTwo;
+    }
+
+    if(operator === "-")
+    {
+        return numOne - numTwo;
+    }
+
+    if(operator === "*")
+    {
+        return numOne * numTwo;
+    }
+
+    if(operator === "/")
+    {
+        return numOne / numTwo;
+    }
+};
+
+const add = function (numOne, numTwo) {
     return num1 + num2;
 };
 
-const subtract = function (num1, num2) {
+const subtract = function (numOne, numTwo) {
     return num1 - num2;
 };
 
-const multiply = function (num1, num2) {
+const multiply = function (numOne, numTwo) {
     return num1 * num2;
 };
 
-const divide = function (num1, num2) {
+const divide = function (numOne, numTwo) {
     return num1 / num2;
 };
