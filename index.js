cefd9396a713e02ab7a7455ce48cbe4d74ec269a
swhasans <SS3858@live.mdx.ac.uk>
2023-06-08 21:19:57 -0400

    Completed #1: Test basic math functions in browser console

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const add = function(num1, num2){
+const add = function (num1, num2) {
     return num1 + num2;
 };
 
-const subtract = function(num1, num2){
+const subtract = function (num1, num2) {
     return num1 - num2;
 };
 
-const multiply = function(num1, num2){
+const multiply = function (num1, num2) {
     return num1 * num2;
 };
 
-const divide = function(num1, num2){
+const divide = function (num1, num2) {
     return num1 / num2;
 };
