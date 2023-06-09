bccf68d9d564faddf4edee5a4014415f520f6e9a
swhasans <SS3858@live.mdx.ac.uk>
2023-06-08 21:17:01 -0400

    Include basic math operators in .js file, HTML skeleton, and add script tag.

---
+++ index.js
@@ -0,0 +1,15 @@
+const add = function(num1, num2){
+    return num1 + num2;
+};
+
+const subtract = function(num1, num2){
+    return num1 - num2;
+};
+
+const multiply = function(num1, num2){
+    return num1 * num2;
+};
+
+const divide = function(num1, num2){
+    return num1 / num2;
+};
