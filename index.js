75b3961c27667e95d43b4f8ed81a3a6e62872710
swhasans <syedwadoodhassan@gmail.com>
2023-07-03 20:44:37 -0400

    Able to log all button values in console

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const numOne = 0;
 const operator = "";
 const numTwo = 0;
+
 const displayValue = 0;
 
-const updateDisplayValue = function (){
-    
+// Selecting the container, input fields, and buttons
+const container = document.querySelector(".container");
+
+const calcDisplay = document.querySelector("#calc-display");
+const userSelectClear = document.querySelector("#user-clear-display");
+
+// const numContainer = document.querySelector(".numberContainer");
+const userSelectNumber = container.querySelectorAll('input');
+
+
+const clearBox = function () {
+    calcDisplay.value = "";
+}
+
+const eraseBox = function () {
+    clearBox();
+}
+
+const updateDisplayValue = function () {
+
 };
 
-const populateDisplay = function (){
+const populateDisplay = function () {
 
 };
 
@@ -29,23 +48,29 @@ const divide = function (numOne, numTwo) {
 
 const operate = function (operator, numOne, numTwo) {
 
-    if(operator === "+")
-    {
+    if (operator === "+") {
         return add(numOne, numTwo);
     }
 
-    if(operator === "-")
-    {
+    if (operator === "-") {
         return subtract(numOne, numTwo);
     }
 
-    if(operator === "*")
-    {
+    if (operator === "*") {
         return multiply(numOne, numTwo);
     }
 
-    if(operator === "/")
-    {
+    if (operator === "/") {
         return divide(numOne, numTwo);
     }
 };
+
+userSelectNumber.forEach(numButton => {
+    numButton.addEventListener('click', (e) => {
+        console.log(e.target.value);
+    });
+});
+
+// Event listener for when the user wants to clear the calculator display
+userSelectClear.addEventListener("click", eraseBox);
+
