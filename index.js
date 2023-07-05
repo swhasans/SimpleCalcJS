cafa4083dbad6cab30bd098d24a0d700e78af00a
swhasans <syedwadoodhassan@gmail.com>
2023-07-04 20:00:19 -0400

    Change const variables to let. Add event listener for user clicking any operation, numbers, equals button. Create function to clear ALL previous equations. To avoid concatenation add Number in add function. Create function to compute result.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const numOne = 0;
-const operator = "";
-const numTwo = 0;
+let numOne = 0;
+let operator = "";
+let numTwo = 0;
 
 let displayValue = 0;
 
@@ -22,38 +22,17 @@ const userSelectNumber = numContainer.querySelectorAll('input');
 //modifyResultContainer
 const userSelectClear = document.querySelector("#user-clear-display");
 const userSelectBksp = document.querySelector("#user-bksp-display");
-
-const clearBox = function () {
-    calcDisplay.value = "";
-}
+const userSelectEquals = document.querySelector("#user-result-display");
 
 const eraseBox = function () {
-    clearBox();
-    displayValue = 0;
-}
-
-const updateDisplayValue = function (event) {
-    calcDisplay.value = calcDisplay.value + event.target.value;
-    displayValue = displayValue + calcDisplay.value;
-    console.log("CalcDisplay Value : " + calcDisplay.value) 
+    calcDisplay.value = "";
+    numOne = 0;
+    operator = "";
+    numTwo = 0;
 };
-
-const populateDisplay = function () {
-
-};
-
-if(userSelectOperation)
-
-userSelectNumber.forEach(numButton => {
-    numButton.addEventListener('click', updateDisplayValue);
-});
-
-// userSelectOperation.forEach(opButton => {
-//     opButton.addEventListener('click', updateDisplayValue);
-// });
 
 const add = function (numOne, numTwo) {
-    return numOne + numTwo;
+    return Number(numOne) + Number(numTwo);
 };
 
 const subtract = function (numOne, numTwo) {
@@ -87,6 +66,56 @@ const operate = function (operator, numOne, numTwo) {
     }
 };
 
+
+const updateNumOneValue = function (event) {
+    numOne = event.target.value;
+};
+
+const updateNumTwoValue = function (event) {
+    numTwo = event.target.value;
+};
+
+const updateOperatorValue = function (event) {
+    operator = event.target.value;
+    eraseBox();
+};
+
+const updateDisplayValue = function (event) {
+    updateNumOneValue( event.target.value);
+    calcDisplay.value += event.target.value;
+    displayValue = calcDisplay.value;
+    
+    console.log("CalcDisplay Value: " + calcDisplay.value);
+    console.log("displayValue: " + displayValue);
+};
+
+const computeValue = function (event) {
+    numTwo = calcDisplay.value;
+    console.log(`numOne: ${numOne} || operator: ${operator} || numTwo: ${numTwo}`);
+    
+    // Make numOne the result
+    numOne = operate(operator, numOne, numTwo);
+    // Update the value displayed in textarea with the result
+    calcDisplay.value = numOne;
+    
+    //Resetting the value of operator and numTwo because a operation has been completed.
+    operator = "";
+    numTwo = 0;
+};
+
+
+// Event listener for when the user wants to select the second number to perform operation on
+userSelectOperation.forEach(opButton => {
+    opButton.addEventListener('click', updateOperatorValue);
+});
+
+// Event listener for when the user wants to select a number from the keypad
+userSelectNumber.forEach(numButton => {
+    numButton.addEventListener('click', updateDisplayValue);
+});
+
+// Event listener for when the user wants to perform a operation with two numbers
+userSelectEquals.addEventListener('click', computeValue);
+
 // Event listener for when the user wants to clear the calculator display
 userSelectClear.addEventListener("click", eraseBox);
-
